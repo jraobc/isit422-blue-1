f45d3b44d6849ace7d986e9ca66ea0c9ee655533
Stephanie McGinty <stephanie.mcginty@bellevuecollege.edu>
2022-09-24 13:49:31 -0700

    node app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
+
+app.get('/', (req, res) => {
+
+});
